a64ddc2bd53e9e01eaf22432461d52f61bd582a1
Paul Kelly <almond64@knights.ucf.edu>
2021-09-04 13:49:19 -0400

    Initial commit failed

---
+++ src/main/java/Solution03.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+
+
+public class Solution03 {
+    public static void main(String[] args) {
+        System.out.println("What is the quote? I came, I saw, I conquered.\nWho said it? Julius Caesar\nJulius Caesar says, \"I came, I saw, I conquered.\"");
+    }
+}
